81adfc1a63791ad44c7cc7c38f2f026b3816d173
Muhammed shifin <muhammedshifin424@gmail.com>
2023-05-21 23:18:34 +0530

    updated hostel admin register form

---
+++ src/Pages/Landing/Locaion.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Locaion from '../../Components/Landing/Location/Locaion'
+
+function Locaion() {
+  return (
+    <div>
+     <Locaion/>
+    </div>
+  )
+}
+
+export default Locaion
--- src/Routes/hostelAdmin.jsx
+++ src/Routes/hostelAdmin.jsx
@@ -1,5 +1,6 @@
 import { Route,Routes } from "react-router-dom";
 import Register from "../Pages/Landing/Register";
+import Locaion from "../Components/Landing/Location/Locaion";
 
 
 
@@ -7,6 +8,7 @@ const hostelRoutes = () => {
     return (
       <Routes>
         <Route path="/register" element={<Register/>}></Route>
+        <Route path="/locaion" element={<Locaion/>}></Route>
       </Routes>
     );
   };
