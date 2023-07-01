d41fe87af5939b340a353e49d563542ec7906085
Muhammed shifin <muhammedshifin424@gmail.com>
2023-07-01 23:40:21 +0530

    added hostelManagment in admin side

--- src/Components/SuperAdmin/HostelManagement.jsx
+++ src/Components/SuperAdmin/HostelManagement.jsx
@@ -1,47 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { hostelStatusApi } from '../../Services/superAdmin';
 
 function HostelManagement() {
-    return (
-        <>
-            <h1 className='text-[#002D7A] text-2xl font-bold '>Hostel Managment</h1>
+  const [hostelData, setHostelData] = useState([]);
 
-            <div>
-                <table className="w-full bg-white rounded-lg overflow-hidden">
-                    <thead className="bg-[#4874BF]">
-                        <tr>
-                            <th className="p-3 text-white text-sm font-bold tracking-wide text-left">NO</th>
-                            <th className="p-3 text-white text-sm font-bold tracking-wide text-left">Hostel Name</th>
-                            <th className="p-3 text-white text-sm font-bold tracking-wide text-left">Email</th>
-                            <th className="p-3 text-white text-sm font-bold tracking-wide text-left">Status</th>
-                            <th className="p-3 text-white text-sm font-bold tracking-wide text-left">Details</th>
-                            <th className="p-3 text-white text-sm font-bold tracking-wide text-left">Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="bg-gray-200">
-                            <td className="p-3 text-gray-500 font-semibold">1</td>
-                            <td className="p-3 text-gray-500 font-semibold">Comfort hostel</td>
-                            <td className="p-3 text-gray-500 font-semibold">derick@gmail.com</td>
-                            <td className="p-3  font-semibold text-red-600">Active</td>
-                            <td className="p-3 font-semibold border-black">
-                                <button className="btn btn-outline btn-info">
-                                    View
-                                </button>
-                            </td>
-                            <td className="p-3 flex flex-col sm:flex-row">
-                                <button className="btn btn-success">
-                                    Approve
-                                </button>
-                                <button className="btn btn-error ml-5">
-                                    Reject
-                                </button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </>
-    )
+  useEffect(() => {
+    const fetchHostelData = async () => {
+      try {
+        const headers = {
+          Authorization: localStorage.getItem('adminToken')
+        };
+
+        const response = await hostelStatusApi(headers);
+        if (response) {
+          console.log(response);
+          setHostelData(response.data);
+        } else {
+          console.log(response);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchHostelData();
+  }, []);
+
+  return (
+    <>
+      <h1 className='text-[#002D7A] text-2xl font-bold '>Hostel Managment</h1>
+
+      <div>
+        {hostelData.length > 0 ? (
+          <table className='w-full bg-white rounded-lg mt-7 overflow-hidden'>
+            <thead className='bg-[#4874BF]'>
+              <tr>
+                <th className='p-3 text-white text-sm font-bold tracking-wide text-left'>NO</th>
+                <th className='p-3 text-white text-sm font-bold tracking-wide text-left'>Hostel Name</th>
+                <th className='p-3 text-white text-sm font-bold tracking-wide text-left'>Email</th>
+                <th className='p-3 text-white text-sm font-bold tracking-wide text-left'>Status</th>
+                <th className='p-3 text-white text-sm font-bold tracking-wide text-left'>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {hostelData.map((data, index) => (
+                <tr key={data._id} className='bg-gray-200'>
+                  <td className='p-3 text-gray-500 font-semibold'>{index + 1}</td>
+                  <td className='p-3 text-gray-500 font-semibold'>{data.hostelName}</td>
+                  <td className='p-3 text-gray-500 font-semibold'>{data.adminData.email}</td>
+                  <td className='p-3 font-semibold text-green-500'>Active</td>
+                  <td className='p-3 flex flex-col sm:flex-row'>
+                    <button className='btn bg-orange-500 text-black hover:bg-orange-400'>Block</button>
+                    <button className='btn btn-error ml-5'>Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>No data available</p>
+        )}
+      </div>
+    </>
+  );
 }
 
-export default HostelManagement
+export default HostelManagement;
--- src/Services/superAdmin.js
+++ src/Services/superAdmin.js
@@ -2,22 +2,27 @@ import axios from '../axios'
 
 
 
-export const adminLoginApi = (formData) =>{
-    return axios.post('/admin/login',formData)
+export const adminLoginApi = (formData) => {
+    return axios.post('/admin/login', formData)
 }
 
 export const hostelRequestApi = (headers) => {
     return axios.get('/admin/hostel-request', { headers });
 };
 
-export const hostelApprovalApi = (id,headers) =>{
-    return axios.patch(`/admin/approve-hostel/${id}`,{},{headers})
+export const hostelApprovalApi = (id, headers) => {
+    return axios.patch(`/admin/approve-hostel/${id}`, {}, { headers })
 }
 
 export const hostelRejectedApi = (id, headers, description) => {
     return axios.patch(`/admin/reject-hostel/${id}`, { description }, { headers });
-  };
-  
+};
+
+export const hostelStatusApi = (headers) =>{
+    return axios.get('/admin/fetchHostelData',{headers})
+}
+
+
 
 
 
