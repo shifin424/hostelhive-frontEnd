0c27e6ed60419c82a8b36da3e0b0a82f6728cf0d
Muhammed shifin <muhammedshifin424@gmail.com>
2023-07-20 11:21:50 +0530

    Solving bug in eslint

--- src/Components/SuperAdmin/Layouts/NavBar.jsx
+++ src/Components/SuperAdmin/Layouts/NavBar.jsx
@@ -1,14 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { FaBars, FaTimes } from "react-icons/fa";
-import image from '../../../assets/images/hostel-logo.png'
-import { Link, useNavigate } from 'react-router-dom';
-import { Button, Drawer, Radio, Space, Menu, Dropdown } from 'antd';
+import image from '../../../assets/images/hostel-logo.png';
+import { Link } from 'react-router-dom';
+import { Menu, Dropdown } from 'antd';
 
 function NavBar() {
-  const [isOpen, setIsOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
-  const navigate= useNavigate()
-  
 
   useEffect(() => {
     const handleResize = () => {
@@ -21,55 +17,40 @@ function NavBar() {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
- 
   if (!isMobile) {
     return null;
   }
 
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const handleLogout =()=>{
-    localStorage.removeItem('adminToken')
-    navigate('/admin/login')
-  }
-
   const mainMenu = (
     <Menu>
-  <Menu.Item key="1">
-    <Link to="/">Home</Link>
-  </Menu.Item>
-  <Menu.Item key="2">
-    <Link to="/">Dashboard</Link>
-  </Menu.Item>
-  <Menu.Item key="3">
-    <Link to="/">Student Requests</Link>
-  </Menu.Item>
-  <Menu.Item key="4">
-    <Link to="/">Hostel Management</Link>
-  </Menu.Item>
-  <Menu.Item key="5">
-    <Link to="/">Logout</Link>
-  </Menu.Item>
-  </Menu>
+      <Menu.Item key="1">
+        <Link to="/">Home</Link>
+      </Menu.Item>
+      <Menu.Item key="2">
+        <Link to="/">Dashboard</Link>
+      </Menu.Item>
+      <Menu.Item key="3">
+        <Link to="/">Student Requests</Link>
+      </Menu.Item>
+      <Menu.Item key="4">
+        <Link to="/">Hostel Management</Link>
+      </Menu.Item>
+      <Menu.Item key="5">
+        <Link to="/">Logout</Link>
+      </Menu.Item>
+    </Menu>
   );
 
   return (
     <div>
- <nav className={`shadow-md bg-white h-16 sticky top-0 w-full ${isMobile ? " " : 'hidden'}`} id="navbar">
+      <nav className={`shadow-md bg-white h-16 sticky top-0 w-full ${isMobile ? ' ' : 'hidden'}`} id="navbar">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://flowbite.com/" className="flex items-center">
+          <Link to="/" className="flex items-center">
             <img src={image} className="h-12 w-28 mr-3" alt="Flowbite Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Kunjutty bite</span>
-          </a>
+          </Link>
           <div className="flex items-center md:order-2">
-            <Dropdown
-              overlay={mainMenu}
-              placement="bottomRight"
-              arrow
-            >
+            <Dropdown overlay={mainMenu} placement="bottomRight" arrow>
               <button
                 data-collapse-toggle="navbar-user"
                 type="button"
@@ -85,69 +66,59 @@ function NavBar() {
                   fill="none"
                   viewBox="0 0 17 14"
                 >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M1 1h15M1 7h15M1 13h15"
-                  />
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
                 </svg>
               </button>
             </Dropdown>
           </div>
-          <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-            id="navbar-user"
-          >
+          <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
-                <a
-                  href="#"
+                <Link
+                  to="/"
                   className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                   aria-current="page"
                 >
                   Home
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
+                <Link
+                  to="/about"
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   About
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
+                <Link
+                  to="/services"
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Services
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
+                <Link
+                  to="/pricing"
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Pricing
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
+                <Link
+                  to="/contact"
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Contact
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
         </div>
       </nav>
-</div>
-
+    </div>
   );
 }
 
