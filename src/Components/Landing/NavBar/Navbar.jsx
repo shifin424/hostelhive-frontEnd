586cc204267a3fbb7e879d229efcc5407c4f6a7f
Muhammed shifin <muhammedshifin424@gmail.com>
2023-07-27 13:38:45 +0530

    changed the navbar

--- src/Components/Landing/NavBar/Navbar.jsx
+++ src/Components/Landing/NavBar/Navbar.jsx
@@ -87,10 +87,10 @@ function Navbar() {
             </svg>
           </button>
         </div>
-        <div
+        {/* <div
           className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-user"
-        >
+         >
           <ul
             className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 dark:border-gray-700"
           >
@@ -136,7 +136,7 @@ function Navbar() {
               </Link>
             </li>
           </ul>
-        </div>
+        </div> */}
       </div>
     </nav>
   );
--- src/Components/Student/Layouts/Navbar.jsx
+++ src/Components/Student/Layouts/Navbar.jsx
@@ -139,7 +139,7 @@ function Navbar() {
           <div
             className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-user"
-          >
+            >
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <Link
